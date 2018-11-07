b30fde0a584cb5b9529c61097faf68d3733a1ba3
ALEXANDER MA COTE <alexcote1rocks@gmail.com>
2018-11-07 12:38:17 -0500

    write register might work but proably not as i do not know how to implement Regdest

--- project.c
+++ project.c
@@ -4,91 +4,108 @@
 /* ALU */
 /* 10 Points */
 //Alexander Cote
-void ALU(unsigned A,unsigned B,char ALUControl,unsigned *ALUresult,char *Zero)
+void ALU(unsigned A, unsigned B, char ALUControl, unsigned *ALUresult, char *Zero)
 {
 
-switch(ALUControl){
+	switch (ALUControl)
+	{
 
-	case 0:
-		*ALUresult=A+B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 1:
-		*ALUresult=A-B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 2:
-		*ALUresult=A<B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 3:
-		*ALUresult=A<B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-		break;
-	case 4:
-		*ALUresult=A&B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 5:
-		*ALUresult=A|B;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 6:
-		*ALUresult=B<<16;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	case 7:
-		*ALUresult=~A;
-		if(*ALUresult==0){
-			*Zero=1;
-		}
-		else{
-			*Zero=0;
-		}
-		break;
-	return;
-}
+		case 0:
+			*ALUresult = A + B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 1:
+			*ALUresult = A - B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 2:
+			*ALUresult = A < B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 3:
+			*ALUresult = A < B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+			break;
+		case 4:
+			*ALUresult = A & B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 5:
+			*ALUresult = A | B;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 6:
+			*ALUresult = B << 16;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+		case 7:
+			*ALUresult = ~A;
+			if (*ALUresult == 0)
+			{
+				*Zero = 1;
+			}
+			else
+			{
+				*Zero = 0;
+			}
+			break;
+			return;
+	}
 }
 
 /* instruction fetch */
 /* 10 Points */
-int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
+int instruction_fetch(unsigned PC, unsigned *Mem, unsigned *instruction)
 {
 
 }
@@ -96,7 +113,9 @@ int instruction_fetch(unsigned PC,unsigned *Mem,unsigned *instruction)
 
 /* instruction partition */
 /* 10 Points */
-void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsigned *r2, unsigned *r3, unsigned *funct, unsigned *offset, unsigned *jsec)
+void
+instruction_partition(unsigned instruction, unsigned *op, unsigned *r1, unsigned *r2, unsigned *r3, unsigned *funct,
+					  unsigned *offset, unsigned *jsec)
 {
 
 }
@@ -105,7 +124,7 @@ void instruction_partition(unsigned instruction, unsigned *op, unsigned *r1,unsi
 
 /* instruction decode */
 /* 15 Points */
-int instruction_decode(unsigned op,struct_controls *controls)
+int instruction_decode(unsigned op, struct_controls *controls)
 {
 
 }
@@ -113,30 +132,31 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* Read Register */
 /* 5 Points */
 //Alexander Cote
-void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
+void read_register(unsigned r1, unsigned r2, unsigned *Reg, unsigned *data1, unsigned *data2)
 {
-*data1=Reg[r1];
-*data2=Reg[r2];
+	*data1 = Reg[r1];
+	*data2 = Reg[r2];
 }
 
 
 /* Sign Extend */
 /* 10 Points */
-void sign_extend(unsigned offset,unsigned *extended_value)
+void sign_extend(unsigned offset, unsigned *extended_value)
 {
 
 }
 
 /* ALU operations */
 /* 10 Points */
-int ALU_operations(unsigned data1,unsigned data2,unsigned extended_value,unsigned funct,char ALUOp,char ALUSrc,unsigned *ALUresult,char *Zero)
+int ALU_operations(unsigned data1, unsigned data2, unsigned extended_value, unsigned funct, char ALUOp, char ALUSrc,
+				   unsigned *ALUresult, char *Zero)
 {
 
 }
 
 /* Read / Write Memory */
 /* 10 Points */
-int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsigned *memdata,unsigned *Mem)
+int rw_memory(unsigned ALUresult, unsigned data2, char MemWrite, char MemRead, unsigned *memdata, unsigned *Mem)
 {
 
 }
@@ -144,22 +164,39 @@ int rw_memory(unsigned ALUresult,unsigned data2,char MemWrite,char MemRead,unsig
 
 /* Write Register */
 /* 10 Points */
-void write_register(unsigned r2,unsigned r3,unsigned memdata,unsigned ALUresult,char RegWrite,char RegDst,char MemtoReg,unsigned *Reg)
+//Alexander Cote
+void write_register(unsigned r2, unsigned r3, unsigned memdata, unsigned ALUresult, char RegWrite, char RegDst,
+					char MemtoReg, unsigned *Reg)
 {
-
+	if (RegWrite == 1)
+	{
+		if (MemtoReg == 1)
+		{
+			Reg[r2] = memdata;
+			Reg[r3] = memdata;
+		}
+		else if (ALUresult == 1)
+		{
+			Reg[r2] = ALUresult;
+			Reg[r3] = ALUresult;
+		}
+	}
+	else
+		return;
 }
 
 /* PC update */
 /* 10 Points */
-void PC_update(unsigned jsec,unsigned extended_value,char Branch,char Jump,char Zero,unsigned *PC)
+void PC_update(unsigned jsec, unsigned extended_value, char Branch, char Jump, char Zero, unsigned *PC)
 {
 
 }
 
-int main(){
-	unsigned *r=malloc(sizeof(unsigned));
-	char *Zero=malloc(sizeof(char));
-	ALU(1,1,1,r,Zero);
+int main()
+{
+	unsigned *r = malloc(sizeof(unsigned));
+	char *Zero = malloc(sizeof(char));
+	ALU(1, 1, 1, r, Zero);
 
 	return 1;
 
