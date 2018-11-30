fa957f26525ae9017a162b97587416157a9cdff0
Charlton Trezevant <ct@ctis.me>
2018-11-30 00:46:57 -0500

    Fixes a bug that causes final results to display incorrectly.

--- tests/test.sh
+++ tests/test.sh
@@ -7,6 +7,7 @@ echo -e "\nCurrent repo status: \n---\n`git status`\n---"
 
 echo -e "\nRemoving any old test artifacts..."
 rm ./tmp/*
+rm -f ./spimtest
 
 echo -e "\nCompiling..."
 gcc -o ./spimtest ../project.c ../spimcore.c
@@ -14,12 +15,13 @@ gcc -o ./spimtest ../project.c ../spimcore.c
 echo -e "\nExecuting with test input..."
 ./spimtest ../input_file.asc <resources/test_input.txt >tmp/test_output
 
-echo -e "\nDiff results: \n---\n`diff tmp/test_output resources/test_output.txt`\n---"
+echo -e "\nDiff results: \n---"
+diff tmp/test_output resources/test_output.txt
 
 if [ $? -eq 0 ]; then
-   echo -e "\nPass! Congratulations :)"
+   echo -e "\n---\nPass! Congratulations :)"
 else
-   echo -e "\nOh no! More work is needed..."
+   echo -e "\n---\nOh no! More work is needed..."
 fi
 
 echo -e "\n=== END TESTING ===\n"
